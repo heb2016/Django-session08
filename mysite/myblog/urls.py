e87d89c1395997a1c338e12349abcc0133c9db7f
He, Beatrice <beatrice.he@cdk.com>
2017-03-06 16:12:10 -0800

    initial commit all

---
+++ mysite/myblog/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from myblog.views import stub_view
+# import the new view
+from myblog.views import list_view
+from myblog.views import detail_view
+
+urlpatterns = [
+  
+    url(r'^$',
+    list_view,  #<-- Change this value from stub_view
+    name="blog_index"),
+
+    url(r'^posts/(?P<post_id>\d+)/$',
+    detail_view,
+    name="blog_detail"),
+
+
+]
